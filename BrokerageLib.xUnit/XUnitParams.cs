9f13adda521ac5d11c304279058cea2a56509a80
ozimax06 <ozi_motaro@yahoo.com>
2018-09-27 18:54:46 +0100

    initial commit

---
+++ BrokerageLib.xUnit/XUnitParams.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using SUT = BrokerageLib;
+
+namespace BrokerageLib.xUnit {
+	public class XUnitParams {
+
+		[Theory]
+		[MemberData("GetTestNumbers")]
+		public void ReturnTrue_WhenThereIsAOddDigit(int candidate) {
+			// arrange
+			var analyzer = new SUT.NumberAnalyzer(candidate);
+
+			// act
+			var result = analyzer.ContainsOddDigit();
+
+			// assert
+			Assert.True(result);
+
+		}
+
+        [Theory]
+        [MemberData("GetTestNumbersToBeCounted")]
+        public void ReturnTrue_WhenNumberContainsTwoDigitFour(int candidate)
+        {
+            // arrange
+            var analyzer = new SUT.NumberAnalyzer(candidate);
+
+            // act
+            var result = analyzer.GetTheCountOfThisDigit(4);
+
+            // assert
+            Assert.Equal(2, result);
+        }
+
+
+        [Fact]
+        [MemberData("GetTestNumbersToBeCounted")]
+        public void ReturnThrow_WhenDividingByZero()
+        {
+            // arrange
+            var analyzer = new SUT.NumberAnalyzer(100);
+
+            // act
+            //var result = analyzer.Divide(0);
+
+            // assert
+            Assert.Throws<DivideByZeroException>(() => analyzer.Divide(0));
+     
+           // Assert.Equal(ex.Message, "Message");
+
+        }
+
+        public static IEnumerable<object> GetTestNumbers() {
+			for (int i = 1; i < 6; i += 2)
+			{
+				yield return new object[] { i };
+			}
+		}
+
+        public static IEnumerable<object> GetTestNumbersToBeCounted()
+        {
+            int[] arr = new int[] { 4400, 4134, 4204, 8443 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                yield return new object[] { arr[i] };
+            }
+        }
+
+    }
+}
---
+++ BrokerageLib/NumberAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokerageLib {
+	public class NumberAnalyzer {
+		private int _candidateNumber;
+		public NumberAnalyzer(int candidateNumber) {
+			_candidateNumber = candidateNumber;
+		}
+		public bool ContainsOddDigit() {
+			int temp;
+			var candidateString = _candidateNumber.ToString();
+
+			foreach (char theChar in candidateString)
+			{
+
+				temp = (int)theChar;
+				if (temp % 2 == 1)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		public int GetTheCountOfThisDigit(int digit) {
+            int temp = _candidateNumber;
+			List<int> digits = new List<int>();
+			while (temp > 0)
+			{
+				digits.Add(temp % 10);
+				temp = temp / 10;
+			}
+
+	
+			return digits.Where(x=>x == digit).Count();
+
+		}
+
+        public int Divide(int number)
+        {
+            var x = _candidateNumber / number;
+            //throw new DivideByZeroException();
+
+            
+
+            return  5;
+        }
+
+	}
+}
